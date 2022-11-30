2e9d109434256d83ec64cfcd98be7524d8ebf57a
Alexxxxxxxxxxxandre <118919108+Alexxxxxxxxxxxandre@users.noreply.github.com>
2022-11-30 12:30:30 +0100

    Update OpticalCommunicationsMain.py

--- OpticalCommunicationsMain.py
+++ OpticalCommunicationsMain.py
@@ -43,7 +43,11 @@ def function_q3(epsilon):
 #So guys the result vary a LOT and it's impossible to get smth near 0 as it will directly go from -10E6 to +10E6 with "only" 100000 different beta. Playing with epsilon around 10E7 I think there are 4 zeros :
 Position=[955,14860,44590,83610]
 
-Modes=[n1*k0+((n1-n2)*k0)*k/100000 for k in Position]
+Modes=[n2*k0+((n1-n2)*k0)*k/100000 for k in Position]
+#Output : [14661718.167858455, 14675586.055429302, 14705236.706093183, 14744152.56175765]
+
+n_eff=[k/k0 for k in Modes]
+#Output : [1.4700955, 1.471486, 1.474459, 1.478361] seems good
 
 
 #plt.plot(pa, J1/(pa*J0), c='black')
